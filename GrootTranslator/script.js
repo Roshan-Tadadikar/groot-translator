907ba2b073c9b661a9d917b4d0d96789c60a2d87
Roshan Tadadikar <71004273+Roshan-Tadadikar@users.noreply.github.com>
2022-11-24 15:46:39 +0530

    Add files via upload

---
+++ GrootTranslator/script.js
@@ -0,0 +1,25 @@
+var btnInput = document.querySelector('#btn-input');
+var input = document.querySelector('#txt-input');
+var output = document.querySelector('#txt-output');
+
+// console.log(text);
+output.innerText = 'Here, translated text will appear';
+
+btnInput.addEventListener('click', () => {
+  translate(input.value);
+});
+
+const URLgenerator = (text) => {
+  return `https://api.funtranslations.com/translate/groot.json?text=${text}`;
+};
+
+const translate = (text) => {
+  fetch(URLgenerator(text))
+    .then((res) => res.json())
+    .then((resJSON) => (output.innerText = resJSON.contents.translated))
+    .catch(
+      (err) =>
+        (output.innerText =
+          'Rate limited, this api is.Again after an hour, try.')
+    );
+};
